d9d079adb2b577f2fecc6ebeefb19aab00ba1e30
JTSenneker <JTSenneker@gmail.com>
2017-08-10 14:25:53 -0400

    Engine Setup
    Setup the Unity Engine for the Project. Created Empty Scenes for the Crypt, Caramelot and Overworld, then made a debug sandbox to work on programming gameplay mechanics.
    
    Also programmed basic player movement.

---
+++ GordonYasmar-ReMasterOfTheGauntlet/Assets/_Scripts/PlayerInputScripts/OverworldControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class OverworldControls : MonoBehaviour {
+   
+
+
+    public float speed = 10;
+
+    private float inputH;
+    private float inputV;
+
+    private CharacterController charController;
+	// Use this for initialization
+	void Start () {
+        charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(inputH, 0, inputV) * speed * Time.deltaTime;
+        charController.Move(movement);
+	}
+}
