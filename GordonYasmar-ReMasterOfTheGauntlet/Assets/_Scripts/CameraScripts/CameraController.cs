5c23660b7200ece83602609035e1ddb82036bbf1
JTSenneker <JTSenneker@gmail.com>
2017-08-11 21:45:11 -0400

    Added Camera System and Reworked Player Movement
    created a script to allow the camera to orbit a target. Also changed how movement works with the player so that the player moves relative to the camera's rotation.

---
+++ GordonYasmar-ReMasterOfTheGauntlet/Assets/_Scripts/CameraScripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform camTarget;
+    public Vector3 posOffset;
+    public float xRot = 50.5f;
+    public float rotSpeed = 75.0f;
+    private float yRot = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        yRot += Input.GetAxis("CameraRotation") * rotSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(xRot, yRot, 0);
+        Vector3 position = rotation * posOffset + camTarget.position;
+
+        transform.rotation = rotation;
+        transform.position = position;
+	}
+}
--- GordonYasmar-ReMasterOfTheGauntlet/Assets/_Scripts/PlayerInputScripts/OverworldControls.cs
+++ GordonYasmar-ReMasterOfTheGauntlet/Assets/_Scripts/PlayerInputScripts/OverworldControls.cs
@@ -4,26 +4,46 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class OverworldControls : MonoBehaviour {
-   
 
 
-    public float speed = 10;
+
+    public float speed = 5;
+    public float grav = -1;
 
     private float inputH;
     private float inputV;
+    private float yRotation;
 
     private CharacterController charController;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         charController = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         inputH = Input.GetAxis("Horizontal");
         inputV = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(inputH, 0, inputV) * speed * Time.deltaTime;
-        charController.Move(movement);
+        if (inputH != 0 || inputV != 0) {
+            RotateY();
+        }
+        
+        Vector3 movement = new Vector3(inputH, 0, inputV);
+        if(movement.magnitude > 1) {
+            movement.Normalize();
+        }
+
+        Vector3 gravity = new Vector3(0, grav, 0);
+        if (charController.isGrounded) gravity.y = 0;
+        else gravity.y = grav;
+
+        charController.Move(gravity + (transform.forward*movement.magnitude*speed*Time.deltaTime));
 	}
+
+    void RotateY() {
+        yRotation = Mathf.Atan2(inputH, inputV)*Mathf.Rad2Deg + Camera.main.transform.rotation.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, yRotation, 0));
+        transform.rotation = rotation;
+    }
 }
